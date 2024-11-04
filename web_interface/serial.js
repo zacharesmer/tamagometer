93dd2aba76e7117210a6a33d3c29a98fa18c5127
zacharesmer <zachresmer@gmail.com>
2024-11-04 14:32:56 -0500

    make listening cancellable

--- web_interface/serial.js
+++ web_interface/serial.js
@@ -7,6 +7,8 @@ let outputStream;
 
 let receivedFromSerial;
 
+let cancelListen = false;
+
 document.getElementById('connect-to-serial').addEventListener('click', () => {
     if (navigator.serial) {
         connectSerial();
@@ -15,24 +17,24 @@ document.getElementById('connect-to-serial').addEventListener('click', () => {
     }
 });
 
-document.getElementById('read-serial').addEventListener('click', () => {
-    const log = document.getElementById('error-message');
-    if (navigator.serial) {
-        try {
-            readSerial();
-        } catch (error) {
-            log.innerText = error;
-        }
-    } else {
-        alert('Web Serial API not supported.');
-    }
-});
+// document.getElementById('read-serial').addEventListener('click', () => {
+//     const log = document.getElementById('error-message');
+//     if (navigator.serial) {
+//         try {
+//             readSerial();
+//         } catch (error) {
+//             log.innerText = error;
+//         }
+//     } else {
+//         alert('Web Serial API not supported.');
+//     }
+// });
 
 document.getElementById('send-serial').addEventListener('click', () => {
     const log = document.getElementById('error-message');
     if (navigator.serial) {
         try {
-            sendCode("0000111000000000110111100101101000011111100010001000100010001000100010001000100000000010000000000010001100000000000000000000000000000000000000000000000000110010");
+            sendCommand("0000111000000000110111100101101000011111100010001000100010001000100010001000100000000010000000000010001100000000000000000000000000000000000000000000000000110010");
         } catch (error) {
             log.innerText = error;
         }
@@ -53,16 +55,28 @@ document.getElementById('reset-serial').addEventListener('click', () => {
 document.getElementById('read-one-command').addEventListener('click', async () => {
     const errorLog = document.getElementById('error-message');
     const log = document.getElementById("output")
-
+    // reset this just in case something else forgot to
+    cancelListen = false;
     if (navigator.serial) {
         try {
-            let command = await readOneCommand();
+            let command = null;
+            while (command === null & !cancelListen) {
+                command = await readOneCommand();
+            }
+            if (cancelListen) {
+                cancelListen = false;
+                return;
+            }
             log.innerText += command.join("");
             log.innerText += "\n\n";
         } catch (error) {
             errorLog.innerText = error;
         }
     }
+});
+
+document.getElementById("cancel-listen").addEventListener("click", () => {
+    cancelListen = true;
 });
 
 async function connectSerial() {
@@ -92,34 +106,36 @@ async function resetSerial(params) {
     sendSerial('\x04');
 }
 
-async function readSerial() {
-    const log = document.getElementById("output")
-    sendSerial("listen");
-    while (true) {
-        const { value, done } = await reader.read();
-        if (value) {
-            // todo: instead of logging, load the serial input into a variable. 
-            // When a newline is received, parse that variable into the different chunks
-            log.textContent += value;
-        }
-        if (done) {
-            console.log('[readLoop] DONE', done);
-            reader.releaseLock();
-            break;
-        }
-    }
-}
+// async function readSerial() {
+//     const log = document.getElementById("output")
+//     sendSerial("listen");
+//     while (true) {
+//         const { value, done } = await reader.read();
+//         if (value) {
+//             // todo: instead of logging, load the serial input into a variable. 
+//             // When a newline is received, parse that variable into the different chunks
+//             log.textContent += value;
+//         }
+//         if (done) {
+//             console.log('[readLoop] DONE', done);
+//             reader.releaseLock();
+//             break;
+//         }
+//     }
+// }
 
 
 async function readOneCommand() {
+    // Returns either null or a string of 1 tamagotchi command.
+
     // match and return a string in the format [PICO]160 1s and 0s[END]
     // out of a stream that may be broken up in arbitrary chunks
-    // This handles a successful code, and a timeout from the pico, but NOT no response from the pico
-    // So, don't unplug it in the middle of this or it will never return.
-    // TODO: implement a client side timeout to handle that case
 
-    // counter for every time a matching character is read from the stream, 
-    // resets on invalid character or end of message (well, the function returns on the end of a valid message)
+    // This handles a successful code, and a timeout from the pico, but NOT no response from the pico
+    // So, don't unplug it in the middle of this. Actually, it's possible the stream 
+    // would end if it's unplug and that's handled.
+
+    // counter, resets on invalid character
     let matching_chars = 0;
 
     const beginning = "[PICO]";
@@ -127,17 +143,19 @@ async function readOneCommand() {
     const end_offset = 160 + beginning.length;
     const total_length = 160 + beginning.length + end.length;
 
-    const timed_out = "timed out"
+    const timed_out_string = "[PICO]timed out[END]";
     let timed_out_matching_chars = 0;
+
 
     let command = [];
     let finished = false;
+    let timed_out = false;
 
     // Tell the board to listen for input
     sendSerial("listen")
 
     while (!finished) {
-        const { value, done } = await reader.read();
+        const { value, done: stream_done } = await reader.read();
         // why am I implementing another finite automaton? maybe I should learn about stream APIs someday
         if (typeof (value) === "string") {
             for (let i = 0; i < value.length; i++) {
@@ -148,7 +166,7 @@ async function readOneCommand() {
                     matching_chars++;
                     // command.push(current);
                 }
-                // if we are in the main body and see a 1 or 0, continue and push it onto whatever we're saving
+                // if we are in the main body and see a 1 or 0, continue and push it onto the thing we're saving
                 else if (matching_chars < end_offset && (current === "0" || current === "1")) {
                     command.push(current);
                     matching_chars++;
@@ -157,37 +175,40 @@ async function readOneCommand() {
                 else if (matching_chars >= end_offset && current === end[matching_chars - end_offset]) {
                     matching_chars++;
                 }
-                // also match the timed out response string, and quit if it matches
-                // TODO check if it matched
-                else if (current === timed_out[timed_out_matching_chars]) {
-                    timed_out_matching_chars++;
-                }
                 // if none of those things happened then the pattern doesn't match.
-                // clear out the recording and wait for a new one
+                // reset the counter and clear out the recording to wait for a new one
                 else {
                     command = [];
                     matching_chars = 0;
                 }
 
-                if (timed_out_matching_chars === timed_out.length) {
-                    throw Error("Microcontroller timed out!");
+                // Check the time out string now
+                // This is in a separate if/else chain because otherwise it will never match since they have the same start
+                if (current === timed_out_string[timed_out_matching_chars]) {
+                    timed_out_matching_chars++;
+                } else {
+                    timed_out_matching_chars = 0;
                 }
-                // If we have matched the total length, the whole string has been matched and we are done!
-                // Or, if we have matched the timeout string, it timed out
+
+
+                // If the time out indicator matched, return null so it can be retried or handled
+                if (timed_out_matching_chars === timed_out_string.length) {
+                    // break out into the while loop. 
+                    // console.log("Timed out")
+                    return null;
+                }
+                // If the whole length has matched, return it
                 if (matching_chars === total_length) {
-                    finished = true;
                     // break out of for loop into while loop
-                    break;
+                    return command;
                 }
             }
         }
-        if (done | finished) {
+        if (stream_done) {
             console.log(`[readCommandLoop] DONE, ${command.join("")}`);
-            break;
+            return null;
         }
     }
-    // reader.releaseLock();
-    return command;
 }
 
 
@@ -201,7 +222,7 @@ async function sendSerial(...lines) {
     writer.releaseLock();
 }
 
-async function sendCode(code) {
+async function sendCommand(code) {
     return await sendSerial("send" + code);
 }
 
@@ -229,14 +250,14 @@ document.getElementById("initiate-visit").addEventListener("click", async () => 
     try {
         console.log(`Sending ${savedMessage1}`)
         // Send message 1
-        sendCode(savedMessage1);
+        sendCommand(savedMessage1);
         // listen for a response
         let response1 = await readOneCommand();
         output.innerText += response1.join("") + "\n"
         console.log(`received ${response1}`)
         // send message 2
         console.log(`Sending ${savedMessage3}`)
-        sendCode(savedMessage3);
+        sendCommand(savedMessage3);
         // listen for a response
         let response2 = await readOneCommand();
         output.innerText += response1.join("") + "\n"
@@ -256,14 +277,14 @@ document.getElementById("wait-for-visit").addEventListener("click", async () => 
         console.log(`received ${response1}`)
         // Send message 1
         console.log(`Sending ${savedMessage2}`)
-        sendCode(savedMessage2);
+        sendCommand(savedMessage2);
         // listen for a response
         let response2 = await readOneCommand();
         output.innerText += response1 + "\n"
         console.log(`received ${response2}`)
         // send message 2
         console.log(`Sending ${savedMessage4}`)
-        sendCode(savedMessage4);
+        sendCommand(savedMessage4);
 
     } catch {
         errorLog.innerText = error;
