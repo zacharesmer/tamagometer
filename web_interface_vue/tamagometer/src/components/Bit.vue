fc143aeb97ab225e0aa3befc2a8eff3f4d409a46
zacharesmer <zachresmer@gmail.com>
2024-11-24 14:58:29 -0500

    make the bits round and use color theme

--- web_interface_vue/tamagometer/src/components/Bit.vue
+++ web_interface_vue/tamagometer/src/components/Bit.vue
@@ -20,17 +20,29 @@ const props = defineProps({
 <style>
 .bit {
     display: inline;
-    padding: 5px;
+    /* padding: 5px; */
     margin: 1px;
-    border: solid;
+
+    height: 2em;
+    width: 2em;
+
+    border-width: 2px;
+    border-style: solid;
+    border-color: var(--dark-blue);
+    border-radius: 50%;
+}
+
+.edited-bit {
+    border-width: 2px;
+    border-style: dotted;
 }
 
 .one {
-    background-color: lightblue;
+    background-color: var(--light-blue);
 
 }
 
 .zero {
-    background-color: papayawhip;
+    background-color: var(--vanilla);
 }
 </style>
