e1bbf182191088879ad258e2ce8101230db50cf5
zacharesmer <zachresmer@gmail.com>
2025-01-02 14:08:34 -0500

    fix jest

--- web_interface_vue/tamagometer/src/serial.ts
+++ web_interface_vue/tamagometer/src/serial.ts
@@ -1,5 +1,7 @@
-export { exportForTesting, getSerialConnection, getPortOrNeedToRetry }
+export { getSerialConnection, getPortOrNeedToRetry }
 export type { SerialConnection }
+
+import { matchCommandString, matchTimedOutString } from "./matchers"
 
 // An object manage the connection to a serial device
 // It is exposed only through `getSerialConnection` because it requires async setup
@@ -215,70 +217,3 @@ async function getPortOrNeedToRetry(): Promise<boolean> {
     }
     return needToRetry
 }
-
-// These could just be methods in the serial connection object. I put them here because I thought it would be easier to test
-// but it may or may not have made a difference.
-function matchCommandString(commandMatched: { matchingChars: number, commandSoFar: string[] }, newString: string): { complete: boolean, commandMatched: { matchingChars: number, commandSoFar: string[] } } {
-    // DIY regex for matching in place
-    // This is kind of ridiculous and I will probably change it to a regular old regex at some point
-    let { matchingChars, commandSoFar } = commandMatched
-    const beginning = "[PICO]";
-    const end = "[END]";
-    const end_offset = 160 + beginning.length;
-    const total_length = 160 + beginning.length + end.length;
-
-    for (let i = 0; i < newString.length; i++) {
-        let current = newString[i].toString();
-        // console.log(`current: ${current}, command so far: ${command}, characters matched: ${matching_chars}`);
-        // if we are in the header and match it, continue
-        if (matchingChars < beginning.length && current === beginning[matchingChars]) {
-            matchingChars++;
-            // command.push(current);
-        }
-        // if we are in the main body and see a 1 or 0, continue and push it onto the thing we're saving
-        else if (matchingChars < end_offset && (current === "0" || current === "1")) {
-            commandSoFar.push(current);
-            matchingChars++;
-        }
-        // if we're in the end and the character matches, continue
-        else if (matchingChars >= end_offset && current === end[matchingChars - end_offset]) {
-            matchingChars++;
-        }
-        // if none of those things happened then the pattern doesn't match.
-        // reset the counter and clear out the recording to wait for a new one
-        else {
-            commandSoFar = [];
-            matchingChars = 0;
-        }
-
-        // If the entire pattern has been matched, return it
-        if (matchingChars === total_length) {
-            return { complete: true, commandMatched: { matchingChars, commandSoFar } }
-        }
-    }
-    // If the entire pattern wasn't matched, return any progress
-    return { complete: false, commandMatched: { matchingChars, commandSoFar } }
-}
-
-function matchTimedOutString(matchingChars: number, newString: string): { complete: boolean, matchingChars: number } {
-    const timedOutString = "[PICO]timed out[END]";
-
-    for (let i = 0; i < newString.length; i++) {
-        let current = newString[i].toString();
-        // In this case, the next character matches
-        if (current === timedOutString[matchingChars]) {
-            matchingChars++;
-        } else {
-            matchingChars = 0;
-        }
-        // If the entire timedOutString has been matched, return
-        if (matchingChars === timedOutString.length) {
-            // console.log("Timed out")
-            return { complete: true, matchingChars };
-        }
-    }
-    // If nothing in newString matched the timedOutString, return the progress (if any)
-    return { complete: false, matchingChars }
-}
-
-const exportForTesting = { matchCommandString, matchTimedOutString }
--- web_interface_vue/tamagometer/src/test/serial.test.ts
+++ web_interface_vue/tamagometer/src/test/serial.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@jest/globals'
-import { exportForTesting } from '../serial';
+import { matchCommandString, matchTimedOutString } from '../matchers';
 
 // Test: If I send partial chunks of a valid message, then they are detected/matched correctly
 test('matchCommandString matches valid message split into chunks', () => {
@@ -11,7 +11,7 @@ test('matchCommandString matches valid message split into chunks', () => {
         commandMatched = { matchingChars: 0, commandSoFar: [] }
         let commandComplete = false;
         for (let chunk of m.chunks) {
-            ({ complete: commandComplete, commandMatched } = exportForTesting.matchCommandString(commandMatched, chunk))
+            ({ complete: commandComplete, commandMatched } = matchCommandString(commandMatched, chunk))
             if (commandComplete) {
                 break
             }
@@ -29,7 +29,7 @@ test('matchTimedOutString matches the timed out string split into chunks', () =>
         let timedOutMatched: { complete: boolean, matchingChars: number }
         timedOutMatched = { complete: false, matchingChars: 0 }
         for (let chunk of m) {
-            timedOutMatched = exportForTesting.matchTimedOutString(timedOutMatched.matchingChars, chunk)
+            timedOutMatched = matchTimedOutString(timedOutMatched.matchingChars, chunk)
             if (timedOutMatched.complete) {
                 break
             }
@@ -41,21 +41,21 @@ test('matchTimedOutString matches the timed out string split into chunks', () =>
 // Test: If I send the timeout string, matchTimedOutString returns true
 test('matchTimedOutString matches the timed out string', () => {
     const timedOutString = "[PICO]timed out[END]"
-    const timedOutMatched = exportForTesting.matchTimedOutString(0, timedOutString)
+    const timedOutMatched = matchTimedOutString(0, timedOutString)
     expect(timedOutMatched.complete).toBe(true)
 })
 
 // Test: If I send the timeout string, matchCommandString returns false
 test("matchCommandString doesn't match the timedout string", () => {
     const timedOutString = "[PICO]timed out[END]"
-    const commandMatched = exportForTesting.matchCommandString({ matchingChars: 0, commandSoFar: [] }, timedOutString)
+    const commandMatched = matchCommandString({ matchingChars: 0, commandSoFar: [] }, timedOutString)
     expect(commandMatched).toMatchObject({ complete: false, commandMatched: { matchingChars: 0, commandSoFar: [] } })
 })
 
 // Test: If I send a valid command string, matchCommandString returns true
 test('matchCommandString matches a valid command', () => {
     const timedOutString = "[PICO]0000111000000001110111100101101000110010100010001000100010001000100010001000100000000000011001000010001100000000000001100000000000000000000000000000101010111000[END]"
-    const commandMatched = exportForTesting.matchCommandString({ matchingChars: 0, commandSoFar: [] }, timedOutString)
+    const commandMatched = matchCommandString({ matchingChars: 0, commandSoFar: [] }, timedOutString)
     expect(commandMatched).toMatchObject({
         complete: true, commandMatched: {
             matchingChars: 171, commandSoFar: ['0', '0', '0', '0', '1', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1', '0', '1', '1', '1', '1', '0', '0', '1', '0', '1', '1', '0', '1', '0', '0', '0', '1', '1', '0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '1', '0', '0', '0', '0', '1', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '1', '0', '1', '1', '1', '0', '0', '0']
@@ -66,7 +66,7 @@ test('matchCommandString matches a valid command', () => {
 // Test: If I send a valid command string, matchTimedOutString returns false
 test("matchTimedOutString doesn't match a valid command string", () => {
     const timedOutString = "[PICO]0000111000000001110111100101101000110010100010001000100010001000100010001000100000000000011001000010001100000000000001100000000000000000000000000000101010111000[END]"
-    const timedOutMatched = exportForTesting.matchTimedOutString(0, timedOutString)
+    const timedOutMatched = matchTimedOutString(0, timedOutString)
     expect(timedOutMatched.complete).toBe(false)
 })
 
@@ -88,10 +88,10 @@ test("matchTimedOutString and matchCommandString don't match things that aren't 
     for (let m of invalidStrings) {
         // let commandMatched: { matchingChars: number, commandSoFar: string[] }
         // commandMatched = { matchingChars: 0, commandSoFar: [] }
-        let commandMatched = exportForTesting.matchCommandString({ matchingChars: 0, commandSoFar: [] }, m)
+        let commandMatched = matchCommandString({ matchingChars: 0, commandSoFar: [] }, m)
         expect(commandMatched.complete).toBe(false)
 
-        let timedOutMatched = exportForTesting.matchTimedOutString(0, m)
+        let timedOutMatched = matchTimedOutString(0, m)
         expect(timedOutMatched.complete).toBe(false)
 
     }
