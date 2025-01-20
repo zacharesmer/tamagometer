71222612e0bfbe4780b4e414ae3bcbd403f7a7cd
zacharesmer <zachresmer@gmail.com>
2025-01-20 18:35:41 -0500

    refactor to get ready for the bootstrap component

--- web_interface_vue/tamagometer/src/components/Record.vue
+++ web_interface_vue/tamagometer/src/components/Record.vue
@@ -1,87 +1,55 @@
 <script lang="ts" setup>
 import AppInputConversationName from './AppInputConversationName.vue';
-import AppStatusIndicator from './AppStatusIndicator.vue';
-import AppButtonRetry from './AppButtonRetry.vue';
+import RecordSnoop from './RecordSnoop.vue';
 
-import { onMounted, ref, useTemplateRef } from 'vue'
-import { onBeforeRouteLeave } from 'vue-router';
+import { ref, useTemplateRef } from 'vue'
 import { toast } from 'vue3-toastify';
 
 import { Conversation } from '@/conversation';
 import { dbConnection } from '@/database';
 import { TamaMessage } from '@/model';
-import { serialWorker, listenContinuously, stopTask, waitForReady, connectSerial } from '@/serial';
-import { portNeedsToBeRequested } from '@/state';
 
-// Store recorded messages as strings
-let snoopOutput = ref(new Array<string>);
-
-// Some test recordings to see the layout
-// snoopOutput.value.push("0000111000000000110111100101101000110010100010001000100010001000100010001000100000000010000000000010001100000000000001100000000000000000000000000000101001010101")
-// snoopOutput.value.push("0000111000000001101111110010001000110001000110010000000000000010000001111000000100000000011001000010001000000000000000000000000000000000000000000000101001010100")
-// snoopOutput.value.push("0000111000001000110111100101101000110010100010001000100010001000100010001000100000000011000000000000000000000000000000000000000000000000000000000000000000101011")
-// snoopOutput.value.push("0000111000001001101111110010001000110001000110010000000000000010000001111000000100000011000000000000000000000000000000000000000000000000000000000000000011001111")
-
-// The retry button is shown when this is true or if a port needs to be requested
-const showRetryButton = ref(false)
 const conversationName = ref("Recorded Conversation")
+const snoopComponent = useTemplateRef("snoop")
 
-const statusIndicator = useTemplateRef("statusIndicator")
+const stagedMessages = ref<{ bitstring: string, recordingID: number }[]>([
+    { bitstring: "", recordingID: NaN },
+    { bitstring: "", recordingID: NaN },
+    { bitstring: "", recordingID: NaN },
+    { bitstring: "", recordingID: NaN },
+])
 
-const stagedMessageIndeces = ref<{ message1: number, message2: number, message3: number, message4: number }>(
-    {
-        message1: NaN,
-        message2: NaN,
-        message3: NaN,
-        message4: NaN
-    })
-
-onMounted(async () => {
-    // Snoop-specific message handling code. More general message handling is in serial.ts
-    serialWorker.addEventListener("message", snoopEventListener)
-    snoop()
-})
-
-onBeforeRouteLeave(async (to, from) => {
-    stopTask().catch(r => { console.log(r) })
-    serialWorker.removeEventListener("message", snoopEventListener)
-})
-
-async function snoop() {
-    showRetryButton.value = false
-    await waitForReady()
-    listenContinuously()
-        .catch(r => { console.log("Snoop stopped :("); showRetryButton.value = true })
+function stageMessage(stagedIndex: number, recordingID: number, bitstring: string) {
+    stagedMessages.value[stagedIndex] = { bitstring, recordingID }
 }
 
-function retry() {
-    connectSerial()
-    snoop()
-}
-
-function snoopEventListener(e: MessageEvent) {
-    const message = e.data as FromWorker
-    switch (message.kind) {
-        case "receivedBitstring": {
-            snoopOutput.value.push(message.bits)
-            break
-        }
-        case "animate": {
-            if (message.animation === "statusIndicator") {
-                statusIndicator.value?.animateStatusIndicator()
-            }
-            break
-        }
+function unstageMessage(stagedIndex: number) {
+    stagedMessages.value[stagedIndex] = { bitstring: "", recordingID: NaN }
+    // tell the recording component to mark the message as unstaged too
+    if (snoopComponent.value != null) {
+        snoopComponent.value.unstageMessage(stagedIndex)
     }
+}
+
+function clearList() {
+    conversationName.value = "Recorded Conversation"
+    for (let i = 0; i < stagedMessages.value.length; i++) {
+        stagedMessages.value[i] = { bitstring: "", recordingID: NaN }
+    }
+    console.log(stagedMessages.value)
+}
+
+function saveName(newName: string) {
+    conversationName.value = newName;
 }
 
 // Create a conversation from the staged messages and store it.
 function saveConversation() {
     // Check if all 4 messages have been selected, return and display an error if not
-    if (Number.isNaN(stagedMessageIndeces.value.message1) ||
-        Number.isNaN(stagedMessageIndeces.value.message2) ||
-        Number.isNaN(stagedMessageIndeces.value.message3) ||
-        Number.isNaN(stagedMessageIndeces.value.message4)) {
+    if (Number.isNaN(stagedMessages.value[0].recordingID) ||
+        Number.isNaN(stagedMessages.value[1].recordingID) ||
+        Number.isNaN(stagedMessages.value[2].recordingID) ||
+        Number.isNaN(stagedMessages.value[3].recordingID)) {
         toast("Could not save: missing messages", {
             autoClose: true,
             closeOnClick: true,
@@ -105,10 +73,10 @@ function saveConversation() {
     }
 
     const fromRecordingConversation = new Conversation(null)
-    fromRecordingConversation.message1 = new TamaMessage(snoopOutput.value[stagedMessageIndeces.value.message1]);
-    fromRecordingConversation.message2 = new TamaMessage(snoopOutput.value[stagedMessageIndeces.value.message2]);
-    fromRecordingConversation.message3 = new TamaMessage(snoopOutput.value[stagedMessageIndeces.value.message3]);
-    fromRecordingConversation.message4 = new TamaMessage(snoopOutput.value[stagedMessageIndeces.value.message4]);
+    fromRecordingConversation.message1 = new TamaMessage(stagedMessages.value[0].bitstring);
+    fromRecordingConversation.message2 = new TamaMessage(stagedMessages.value[1].bitstring);
+    fromRecordingConversation.message3 = new TamaMessage(stagedMessages.value[2].bitstring);
+    fromRecordingConversation.message4 = new TamaMessage(stagedMessages.value[3].bitstring);
     fromRecordingConversation.name = conversationName.value;
 
     const toastId = toast("Saving...")
@@ -118,121 +86,22 @@ function saveConversation() {
         })
         .catch(r => { toast.update(toastId, { render: "Error", type: "error" }) })
 }
-
-function saveName(newName: string) {
-    conversationName.value = newName;
-}
-
-function clearList() {
-    snoopOutput.value = []
-    conversationName.value = "Recorded Conversation"
-    stagedMessageIndeces.value = {
-        message1: NaN,
-        message2: NaN,
-        message3: NaN,
-        message4: NaN,
-    }
-}
-
 </script>
 
 <template>
-    <AppButtonRetry v-if="showRetryButton || portNeedsToBeRequested" direction="column" @retry="retry"></AppButtonRetry>
-    <div v-else>
-        <div class="recording-body-container">
-            <!-- TODO: refactor this recording-list div into its own component that handles the webworker stuff 
-             and emits signals with the recordings. This will allow swapping in a different component, like the
-             planned bootstrapping one. -->
-            <div class="recording-list">
-                <div class="title">
-                    <AppStatusIndicator ref="statusIndicator"></AppStatusIndicator>
-                    <h2>Listening for input...</h2>
-                </div>
-                <div v-if="snoopOutput.length > 0" class="recording-table-container">
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th></th>
-                                <th>Set as message</th>
-                                <th></th>
-                            </tr>
-                            <template v-for="(message, index) in snoopOutput">
-                                <tr>
-                                    <td>{{ index }}</td>
-                                    <td>
-                                        <div class="set-message-buttons-container">
-                                            <button class="round-button"
-                                                :class="{ 'active-message-set-button': (index === stagedMessageIndeces.message1) }"
-                                                @click="() => { stagedMessageIndeces.message1 = index }">1</button>
-                                            <button class="round-button"
-                                                :class="{ 'active-message-set-button': (index === stagedMessageIndeces.message2) }"
-                                                @click="() => { stagedMessageIndeces.message2 = index }">2</button>
-                                            <button class="round-button"
-                                                :class="{ 'active-message-set-button': (index === stagedMessageIndeces.message3) }"
-                                                @click="() => { stagedMessageIndeces.message3 = index }">3</button>
-                                            <button class="round-button"
-                                                :class="{ 'active-message-set-button': (index === stagedMessageIndeces.message4) }"
-                                                @click="() => { stagedMessageIndeces.message4 = index }">4</button>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div class="bitstring">{{ message }}</div>
-                                    </td>
-                                </tr>
-                            </template>
-                        </tbody>
-                    </table>
-                </div>
-                <button v-if="snoopOutput.length > 0" @click="() => { clearList() }"
-                    class="icon-label-button">Clear</button>
-            </div>
-            <div class="staged-messages-container">
-                <AppInputConversationName :name="conversationName" @save-name="(newName) => { saveName(newName) }"
-                    @save-new-conversation="saveConversation" class="title">
-                </AppInputConversationName>
-                <div class="message-label-container">
-                    <button class="message-label round-button"
-                        :class="{ 'message-label-set': (!Number.isNaN(stagedMessageIndeces.message1)) }"
-                        @click="stagedMessageIndeces.message1 = NaN">
-                        1
-                    </button>
-                    <div class="message from-tama1">
-                        {{ Number.isNaN(stagedMessageIndeces.message1) ? "" : snoopOutput[stagedMessageIndeces.message1]
-                        }}
-                    </div>
-                </div>
-                <div class="message-label-container">
-                    <div class="message from-tama2">
-                        {{ Number.isNaN(stagedMessageIndeces.message2) ? "" : snoopOutput[stagedMessageIndeces.message2]
-                        }}
-                    </div>
-                    <button class="message-label round-button"
-                        :class="{ 'message-label-set': (!Number.isNaN(stagedMessageIndeces.message2)) }"
-                        @click="stagedMessageIndeces.message2 = NaN">
-                        2
-                    </button>
-                </div>
-                <div class="message-label-container">
-                    <button class="message-label round-button"
-                        :class="{ 'message-label-set': (!Number.isNaN(stagedMessageIndeces.message3)) }"
-                        @click="stagedMessageIndeces.message3 = NaN">
-                        3
-                    </button>
-                    <div class="message from-tama1">
-                        {{ Number.isNaN(stagedMessageIndeces.message3) ? "" : snoopOutput[stagedMessageIndeces.message3]
-                        }}
-                    </div>
-                </div>
-                <div class="message-label-container">
-                    <div class="message from-tama2">
-                        {{ Number.isNaN(stagedMessageIndeces.message4) ? "" : snoopOutput[stagedMessageIndeces.message4]
-                        }}
-                    </div>
-                    <button class="message-label round-button"
-                        :class="{ 'message-label-set': (!Number.isNaN(stagedMessageIndeces.message4)) }"
-                        @click="stagedMessageIndeces.message4 = NaN">
-                        4
-                    </button>
+    <div class="recording-body-container">
+        <RecordSnoop @stage-message="stageMessage" @clear-list="() => { clearList() }" ref="snoop" />
+        <div class="staged-messages-container">
+            <AppInputConversationName :name="conversationName" @save-name="(newName) => { saveName(newName) }"
+                @save-new-conversation="saveConversation" class="title">
+            </AppInputConversationName>
+            <div v-for="(n, index) in 4" class="message-label-container"
+                :class="{ 'swap-button-order': (index % 2 == 0) }">
+                <button class="message-label round-button"
+                    :class="{ 'message-label-set': (!Number.isNaN(stagedMessages[index].recordingID)) }"
+                    @click="() => { unstageMessage(index) }">{{ n }}</button>
+                <div :class="(index % 2 == 0) ? 'message from-tama1' : 'message from-tama2'">
+                    {{ stagedMessages[index].bitstring }}
                 </div>
             </div>
         </div>
@@ -250,13 +119,6 @@ h2 {
     text-align: center;
 }
 
-.recording-list {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1rem;
-}
-
 .recording-body-container {
     display: flex;
     flex-direction: row;
@@ -264,23 +126,6 @@ h2 {
     gap: 2rem 10rem;
     align-items: start;
     justify-content: space-around;
-}
-
-th {
-    position: sticky;
-    top: 0;
-}
-
-.recording-table-container {
-    max-height: 70vh;
-    overflow-y: auto;
-}
-
-.bitstring {
-    max-width: 40ch;
-    font-family: monospace;
-    text-wrap: wrap;
-    word-wrap: break-word;
 }
 
 .set-message-buttons-container {
@@ -297,7 +142,6 @@ th {
 .active-message-set-button {
     background-color: var(--pink);
 }
-
 
 .staged-messages-container {
     display: flex;
@@ -328,5 +172,9 @@ th {
     display: flex;
     gap: 1rem;
     align-items: end;
+}
+
+.swap-button-order {
+    flex-direction: row-reverse;
 }
 </style>
---
+++ web_interface_vue/tamagometer/src/components/RecordSnoop.vue
@@ -0,0 +1,195 @@
+<script lang="ts" setup>
+import AppStatusIndicator from './AppStatusIndicator.vue'
+import AppButtonRetry from './AppButtonRetry.vue';
+
+import { ref, useTemplateRef, onMounted, onBeforeUnmount } from 'vue'
+
+import { serialWorker, stopTask, waitForReady, listenContinuously, connectSerial } from '@/serial'
+import { portNeedsToBeRequested } from '@/state';
+
+// Store recorded messages as strings
+let snoopOutput = ref(new Array<string>);
+
+// The recordingID for each message that is staged.
+const stagedMessageIDs = ref([NaN, NaN, NaN, NaN])
+const statusIndicator = useTemplateRef("statusIndicator")
+// The retry button is shown when this is true or if a port needs to be requested
+const showRetryButton = ref(false)
+
+// Send messages to update the parent component. This is necessary because there 
+// will be other components that can be used instead to record in different ways.
+const emit = defineEmits<{
+    (e: 'stageMessage',
+        stagedIndex: number, // which message this should be in the conversation
+        recordingID: number, // In this case,the index of the array the recording is stored in
+        bitstring: string // the bitstring for the message
+    ): void,
+    (e: 'clearList'
+    ): void
+}>()
+
+defineExpose({ unstageMessage })
+
+function stageMessage(recordingID: number, stagedIndex: number) {
+    if (stagedIndex < 0 || stagedIndex > 3) {
+        throw Error("Invalid index, can only stage a message at index 0, 1, 2, or 3")
+    }
+    stagedMessageIDs.value[stagedIndex] = recordingID
+    emit("stageMessage", stagedIndex, recordingID, snoopOutput.value[recordingID])
+}
+
+function unstageMessage(stagedIndex: number) {
+    if (stagedIndex < 0 || stagedIndex > 3) {
+        throw Error("Invalid index, can only stage a message at index 0, 1, 2, or 3")
+    }
+    stagedMessageIDs.value[stagedIndex] = NaN
+}
+
+function clearList() {
+    snoopOutput.value = []
+    stagedMessageIDs.value = [NaN, NaN, NaN, NaN]
+    emit("clearList")
+}
+
+onMounted(async () => {
+    // Snoop-specific message handling code. More general message handling is in serial.ts
+    serialWorker.addEventListener("message", snoopEventListener)
+    snoop()
+    // Some test recordings to see the layout
+    snoopOutput.value.push("0000111000000000110111100101101000110010100010001000100010001000100010001000100000000010000000000010001100000000000001100000000000000000000000000000101001010101")
+    snoopOutput.value.push("0000111000000001101111110010001000110001000110010000000000000010000001111000000100000000011001000010001000000000000000000000000000000000000000000000101001010100")
+    snoopOutput.value.push("0000111000001000110111100101101000110010100010001000100010001000100010001000100000000011000000000000000000000000000000000000000000000000000000000000000000101011")
+    snoopOutput.value.push("0000111000001001101111110010001000110001000110010000000000000010000001111000000100000011000000000000000000000000000000000000000000000000000000000000000011001111")
+    stageMessage(0, 0)
+    stageMessage(1, 1)
+    stageMessage(2, 2)
+    stageMessage(3, 3)
+})
+
+// TODO: this was OnBeforeRouteLeave, did that break anything
+onBeforeUnmount(() => {
+    stopTask().catch(r => { console.log(r) })
+    serialWorker.removeEventListener("message", snoopEventListener)
+})
+
+async function snoop() {
+    showRetryButton.value = false
+    await waitForReady()
+    listenContinuously()
+        .catch(r => { console.log("Snoop stopped :("); showRetryButton.value = true })
+}
+
+function retry() {
+    connectSerial()
+    snoop()
+}
+
+function snoopEventListener(e: MessageEvent) {
+    const message = e.data as FromWorker
+    switch (message.kind) {
+        case "receivedBitstring": {
+            snoopOutput.value.push(message.bits)
+            break
+        }
+        case "animate": {
+            if (message.animation === "statusIndicator") {
+                statusIndicator.value?.animateStatusIndicator()
+            }
+            break
+        }
+    }
+}
+
+</script>
+
+<template>
+    <AppButtonRetry v-if="showRetryButton || portNeedsToBeRequested" direction="column" @retry="retry"></AppButtonRetry>
+    <div v-else>
+        <div class="recording-list">
+            <div class="title">
+                <AppStatusIndicator ref="statusIndicator"></AppStatusIndicator>
+                <h2>Listening for input...</h2>
+            </div>
+            <div v-if="snoopOutput.length > 0" class="recording-table-container">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th></th>
+                            <th>Set as message</th>
+                            <th></th>
+                        </tr>
+                        <template v-for="(message, messageIndex) in snoopOutput">
+                            <tr>
+                                <td>{{ messageIndex }}</td>
+                                <td>
+                                    <div class="set-message-buttons-container">
+                                        <!-- n is 1-based, buttonIndex is 0 based -->
+                                        <button v-for="(n, buttonIndex) in 4" class="round-button"
+                                            :class="{ 'active-message-set-button': (messageIndex === stagedMessageIDs[buttonIndex]) }"
+                                            @click="() => { stageMessage(messageIndex, buttonIndex) }">{{ n }}</button>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="bitstring">{{ message }}</div>
+                                </td>
+                            </tr>
+                        </template>
+                    </tbody>
+                </table>
+            </div>
+            <button v-if="snoopOutput.length > 0" @click="() => { clearList() }"
+                class="icon-label-button">Clear</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.title {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+}
+
+h2 {
+    text-align: center;
+}
+
+.recording-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+}
+
+th {
+    position: sticky;
+    top: 0;
+}
+
+.recording-table-container {
+    max-height: 70vh;
+    overflow-y: auto;
+}
+
+.bitstring {
+    max-width: 40ch;
+    font-family: monospace;
+    text-wrap: wrap;
+    word-wrap: break-word;
+}
+
+.set-message-buttons-container {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    align-items: baseline;
+}
+
+.round-button {
+    font-size: large;
+}
+
+.active-message-set-button {
+    background-color: var(--pink);
+}
+</style>
